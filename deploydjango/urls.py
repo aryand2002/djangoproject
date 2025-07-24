3805664155118e316c5113404a7c147cc4fd86a6
aryand7265@gmail.com <aryand7265@gmail.com>
2025-07-24 16:17:08 +0530

    admin theme changed

--- deploydjango/urls.py
+++ deploydjango/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from django.http import HttpResponse
 from django.urls import path, include
 
+
 def home(request):
     return HttpResponse("<h1>Hello from Render Django!</h1>")
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('', home),
     path('admin/', admin.site.urls),
-    path('jet/', include('jet.urls', 'jet')),
+    
 ]
