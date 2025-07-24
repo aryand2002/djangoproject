380b5c89c3ef8091a5debf69678e61cd05b9857c
aryand7265@gmail.com <aryand7265@gmail.com>
2025-07-24 15:28:01 +0530

    super user created

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+python create_superuser.py
---
+++ create_superuser.py
@@ -0,0 +1,10 @@
+# create_superuser.py
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("aryan", "aryan@gmail.com", "aryan")
+    print("Superuser created successfully.")
+else:
+    print("Superuser already exists.")
