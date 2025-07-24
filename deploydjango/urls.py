acc5dc60fb9caff03e122d3006948f28773d944e
aryand7265@gmail.com <aryand7265@gmail.com>
2025-07-24 16:09:03 +0530

    admin theme changed

--- deploydjango/urls.py
+++ deploydjango/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from django.urls import path, include
 
 def home(request):
     return HttpResponse("<h1>Hello from Render Django!</h1>")
@@ -8,4 +9,5 @@ def home(request):
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
+    path('jet/', include('jet.urls', 'jet')),
 ]
