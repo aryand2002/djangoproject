dd69c364799358aba06a1aed2cebbadd0d8cb76e
aryand7265@gmail.com <aryand7265@gmail.com>
2025-07-29 14:50:01 +0530

    some changes

--- myapp/views.py
+++ myapp/views.py
@@ -102,7 +102,7 @@ class BlogPostAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk):
+    def patch(self, request, pk):
         try:
             post = BlogPost.objects.get(pk=pk)
         except BlogPost.DoesNotExist:
@@ -144,7 +144,7 @@ class CatalogItemAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk):
+    def patch(self, request, pk):
         try:
             item = CatalogItem.objects.get(pk=pk)
         except CatalogItem.DoesNotExist:
@@ -163,6 +163,7 @@ class CatalogItemAPIView(APIView):
             return Response({'message': 'Deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
         except CatalogItem.DoesNotExist:
             return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
 
 class GalleryView(APIView):
     def get(self, request, pk=None):
@@ -185,7 +186,7 @@ class GalleryView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk):
+    def patch(self, request, pk):
         try:
             item = Gallery.objects.get(pk=pk)
         except Gallery.DoesNotExist:
@@ -204,4 +205,5 @@ class GalleryView(APIView):
             return Response({'message': 'Deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
         except Gallery.DoesNotExist:
             return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
 
