b624444c9b0993a026ad79a3dadf791e924155a1
aryand7265@gmail.com <aryand7265@gmail.com>
2025-07-29 14:59:53 +0530

    some changes

--- myapp/views.py
+++ myapp/views.py
@@ -101,18 +101,18 @@ class BlogPostAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
     def patch(self, request, pk):
-        try:
-            post = BlogPost.objects.get(pk=pk)
-        except BlogPost.DoesNotExist:
-            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+            try:
+                post = BlogPost.objects.get(pk=pk)
+            except BlogPost.DoesNotExist:
+                return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = BlogPostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer = BlogPostSerializer(post, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         try:
